969034ddb60e299a9483bd095ccb4642cb47db56
Ayaat2012 <iffatrajshahi@gmail.com>
2026-01-03 20:51:32 +0600

    initial commit

--- Project-3.py
+++ Project-3.py
@@ -7,13 +7,3 @@ df = pd.read_csv('Company Sales Data.csv')
 print(df)
 
 # Creating a line-plot for displaying the month-wise profit of the company
-def monthlyProfit():
-    plt.plot('total_profit', 'month_number', marker = 'o', mec = 'r', linestyle = 'dotted', color = 'black', linewidth = 3)
-    plt.title("Monthly Profit")
-    plt.xlabel("Month")
-    plt.ylabel("Profit")
-    plt.grid(True)
-    plt.tight_layout()
-    plt.show()
-
-monthlyProfit()
